b8b5453897dbf2be325c90cdd9bbed47cb410a7d
jerin <jerin@Jerin-Alex>
2017-09-30 20:55:45 +0530

    #ui changes

--- masterpage.php
+++ masterpage.php
@@ -8,7 +8,7 @@
                 <div class="content">
                     <div class="carousel carousel-slider center" data-indicators="true">
                         <div class="carousel-fixed-item center">
-                            <a href="#skill" class="btn waves-effect blue white-text">Get Started</a>
+                            <a href="#skill" class="btn waves-effect waves-light blue white-text">Get Started</a>
                         </div>
                         <div class="carousel-item white-text img-1" href="#one!">
                             <p>Man is still the most extraordinary computer of all.</p>
@@ -37,64 +37,159 @@
                 <div class="content-2">
                     <h3>Technical Skill</h3>
                     <div class="row">
-                        <div class="col s12">
-                            <div class="card teal darken-3">
-                                <div class="card-content white-text">
-                                    <span class="card-title"><i class="fa fa-language" aria-hidden="true"></i>&nbsp;Languages</span>
-                                    <ol>
-                                        <p class="lan-icon"><i class="icon-csharp"></i>.Net</p>
-                                        <p class="lan-icon"><i class="icon-php-alt"></i>Php</p>
-                                        <p class="lan-icon"><i class="icon-javascript"></i>Javascript</p>
-                                        <p class="lan-icon"><i class="icon-java"></i>Core Java</p>
-                                        <p class="lan-icon"><i class="icon-database"></i>SQL</p>
-                                    </ol>
+                        <div class="col s12 teal darken-3">
+                            <span class="heading-cls"><i class="fa fa-language" aria-hidden="true"></i>&nbsp;Languages</span>
+                            <div class="row">
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-csharp"></i>.Net</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-php-alt"></i>Php</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-javascript"></i>Javascript</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-java"></i>Core Java</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col s12">
-                            <div class="card teal darken-3">
-                                <div class="card-content white-text">
-                                    <span class="card-title"><i class="fa fa-star" aria-hidden="true"></i>&nbsp;Skills</span>
-                                    <ol>
-                                        <p class="lan-icon"><i class="icon-html5-alt"></i>Html</p>
-                                        <p class="lan-icon"><i class="icon-css3-alt"></i>Css</p>
-                                        <p class="lan-icon"><i class="icon-jquery"></i>jquery</p>
-                                        <p class="lan-icon"><i class="icon-bootstrap"></i>Bootstrap</p>
-                                        <p class="lan-icon">Ajax</p>
-                                        <p class="lan-icon"><i class="icon-mysql-alt"></i>Mysql</p>
-                                        <p class="lan-icon"><i class="icon-mysql-alt"></i>Mysqli</p>
-                                        <p class="lan-icon">PDO</p>
-                                        <p class="lan-icon">Slim</p>
-                                        <p class="lan-icon">NOTORM</p>
-                                    </ol>
+                        <div class="col s12 teal darken-3">
+                            <span class="heading-cls"><i class="fa fa-star" aria-hidden="true"></i>&nbsp;Skills</span>
+                            <div class="row">
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-html5-alt"></i>Html</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-css3-alt"></i>Css</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-jquery"></i>jquery</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-bootstrap"></i>Bootstrap</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon">Ajax</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon">PDO</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon">Slim</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l3">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon">NOTORM</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col s12 m6 l6">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-mysql-alt"></i>Mysql</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l6">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-mysql-alt"></i>Mysqli</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col s12">
-                            <div class="card teal darken-3">
-                                <div class="card-content white-text">
-                                    <span class="card-title"><i class="icon-database-alt"></i>&nbsp;Database</span>
-                                    <ol>
-                                        <p class="lan-icon"><i class="icon-mssql"></i>Microsoft Sql Server</p>
-                                        <p class="lan-icon"><i class="icon-mysql"></i>Mysql</p>
-                                    </ol>
+                        <div class="col s12 teal darken-3">
+                            <span class="heading-cls"><i class="icon-database-alt"></i>&nbsp;Database</span>
+                            <div class="row">
+                                <div class="col s12 m6 l6">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-mssql"></i>Microsoft Sql Server</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l6">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-mysql"></i>Mysql</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col s12">
-                            <div class="card teal darken-3">
-                                <div class="card-content white-text">
-                                    <span class="card-title"><i class="fa fa-desktop"></i>&nbsp;Operating System</span>
-                                    <ol>
-                                        <p class="lan-icon"><i class="fa fa-windows"></i>Windows 10</p>
-                                        <p class="lan-icon"><i class="icon-ubuntu"></i>Ubuntu</p>
-                                    </ol>
+                        <div class="col s12 teal darken-3">
+                            <span class="heading-cls"><i class="fa fa-desktop"></i>&nbsp;Operating System</span>
+                            <div class="row">
+                                <div class="col s12 m6 l6">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="fa fa-windows"></i>Windows 7,8.1,10</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l6">
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <p class="lan-icon"><i class="icon-ubuntu"></i>Ubuntu</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -103,7 +198,7 @@
             </div>
         </div>
         <div class="fixed-action-btn horizontal">
-            <a class="btn-floating btn-large waves-effect waves-light hide red upward" onclick="goTop(this)">
+            <a class="btn-floating btn-large waves-effect waves-light hide grey darken-1 upward" onclick="goTop(this)">
                 <i class="large material-icons">arrow_upward</i>
             </a>
         </div>
