ca0c08d109f9fe6c8986aec9164a00424630c480
jerin <jerin@Jerin-Alex>
2017-10-04 15:03:29 +0530

    #added link to github

--- masterpage.php
+++ masterpage.php
@@ -406,7 +406,7 @@
                         <ul class="animated fadeInUp">
                             <li><a class="btn-floating indigo waves-effect waves-light p-i-cls" href="#!"><i class="fa fa-facebook-square"></i></a></li><br />
                             <li><a class="btn-floating red darken-2 waves-effect waves-light p-i-cls" href="#!"><i class="fa fa-google-plus-circle"></i></a></li><br />
-                            <li><a class="btn-floating grey darken-2 waves-effect waves-light p-i-cls" href="#!"><i class="fa fa-github"></i></a></li><br />
+                            <li><a class="btn-floating grey darken-2 waves-effect waves-light p-i-cls" href="https://github.com/gabrieljerin"><i class="fa fa-github"></i></a></li><br />
                             <li><a class="btn-floating blue lighten-1 waves-effect waves-light p-i-cls" href="#!"><i class="fa fa-linkedin-square"></i></a></li>
                         </ul>
                     </div>
